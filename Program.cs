8e06be71b99154595b8ace9a746dc9392d42eddc
ibrahimcakmakk <ibrahim.cakmak@stu.fsm.edu.tr>
2022-08-18 14:32:23 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+class Program{
+
+    static void Main(string[] args){
+
+        //sort
+        int[] sayiDizisi = {23,12,4,86,72,3,11,17};
+
+        Console.WriteLine("sırasız dizi");
+        foreach (var item in sayiDizisi)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("sıralı dizi");
+        Array.Sort(sayiDizisi);
+
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+        //clear
+        Console.WriteLine("array clear");
+        Array.Clear(sayiDizisi,2,2);
+
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+        //reverse
+        Console.WriteLine("array reverse");
+        Array.Reverse(sayiDizisi);
+
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+        //indexOf
+        Console.WriteLine("array indexOf");
+        Array.IndexOf(sayiDizisi,23);
+        Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+        //resize
+        Console.WriteLine("array resize");
+        Array.Resize<int>(ref sayiDizisi,9);
+        sayiDizisi[8] = 99;
+
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+    }
+}
